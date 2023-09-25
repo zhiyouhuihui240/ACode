b20bcc5c81dab1d94e3fba3c6a74c7d5f866e655
PangZhiYou <pangzhiyou@qmlke.com>
2023-09-25 17:27:36 +0800

    debug17

--- library/src/main/groovy/cn/hx/plugin/junkcode/utils/JunkUtil.groovy
+++ library/src/main/groovy/cn/hx/plugin/junkcode/utils/JunkUtil.groovy
@@ -127,7 +127,7 @@ class JunkUtil {
 //                        .addStatement("\$T.logg()", "${Utils.class}")
 //                        .addStatement("\$T.$str()", "${fullName}")
 //                        .addStatement("\$T.$str()", "${cn.hx.plugin.junkcode.utils.Utils.class}")
-                        .addStatement("${fullName}")
+                        .addStatement("\$T.$str()", "${Class.forName((fullName))}")
                         .addStatement("${fullName.class}")
                         .addStatement("${fullName.metaClass}")
                         .addStatement("${fullName.getClass()}")
