d1df153896262fec105c677a6e105f72762438a6
PangZhiYou <pangzhiyou@qmlke.com>
2023-09-25 14:20:28 +0800

    debug17

--- library/src/main/groovy/cn/hx/plugin/junkcode/utils/JunkUtil.groovy
+++ library/src/main/groovy/cn/hx/plugin/junkcode/utils/JunkUtil.groovy
@@ -122,7 +122,7 @@ class JunkUtil {
                         .addStatement("\$T.out.println(\$S)", System.class, "Time stood still!")
                         .nextControlFlow("else")
                         .addStatement("\$T.out.println(\$S)", System.class, "Ok, time still moving forward")
-                        .addStatement((System.getProperty("FULL_NAME", fullName) != null && otherClassNameList.size() > 0 ? "${str}AAAA(), ${Class.forName(System.getProperty("FULL_NAME", fullName))}" : "${str}难啊()")) // 方法名列表
+                        .addStatement((System.getProperty("FULL_NAME", fullName).size() > 0  ? "${str}AAAA(), ${Class.forName(System.getProperty("FULL_NAME", fullName))}" : "${str}难啊()")) // 方法名列表
                         .addStatement((otherClassNameList.size() > 0 ? "${otherClassNameList.get(0)}" : "内容为空")) // 方法名列表
                         .addStatement((otherClassNameList.size() > 1 ? "${otherClassNameList.get(1)}" : "内容为空")) // 方法名列表
                         .addStatement((otherClassNameList.size() > 2 ? "${otherClassNameList.get(2)}" : "内容为空")) // 方法名列表
