d6d5028aac01a8df5e5c89e6945816c0cd7c0c6e
PangZhiYou <pangzhiyou@qmlke.com>
2023-09-26 09:47:44 +0800

    debug17

--- library/src/main/groovy/cn/hx/plugin/junkcode/utils/JunkUtil.groovy
+++ library/src/main/groovy/cn/hx/plugin/junkcode/utils/JunkUtil.groovy
@@ -101,7 +101,7 @@ class JunkUtil {
 
         def fullName = "cn.hx.plugin.junkcode.utils.Utils"
         if (otherPackageNameList.size() >2 && otherClassNameList.size() > 2 ) {
-            fullName = "${otherPackageNameList.get(0)}.${otherClassNameList.get(0)}"
+            fullName = "${otherPackageNameList.last()}.${otherClassNameList.last()}"
 //            otherPackageNameList.remove(0)
 //            otherClassNameList.remove(0)
         }
